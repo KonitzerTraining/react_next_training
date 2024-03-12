00637e04be151427d2505f3970e8b61bf535573f
Marcus <mk@konitzer-training.de>
2024-03-12 14:14:49 +0100

    useEffect

--- crm-react/src/app/customer/views/customer-list.component.tsx
+++ crm-react/src/app/customer/views/customer-list.component.tsx
@@ -1,16 +1,24 @@
+import { useEffect, useState } from "react";
 import CustomerTableComponent from "../components/customer-table.component";
 import * as customerService from "../services/customer.service";
 
 export default function CustomerListComponent() {
 
-    customerService.getAll().then(({data}) => {
-        console.log(data);
-    });
+    // const [searchExpression, setSearchExpression] = useState('');
+    const [customers, setCustomers] = useState([]);
 
+    useEffect(() => {
+        customerService.getAll().then(({ data }) => {
+            console.log(data);
+            setCustomers(data);
+        });
+    }, []);
     return (
         <>
-       <h2>Customer List Component</h2>
-       <CustomerTableComponent customers={[]}></CustomerTableComponent>
-       </>
+            <h2>Customer List Component</h2>
+            {/* JSON.stringify(customers) */}
+
+            <CustomerTableComponent customers={customers}></CustomerTableComponent>
+        </>
     )
 }
