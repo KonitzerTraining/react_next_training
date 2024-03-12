33ce285915354e103aea5e414f7f7f9e880e3de2
Marcus <mk@konitzer-training.de>
2024-03-12 11:46:46 +0100

    map und if

--- crm-react/src/app/template-syntax/template-syntax.page.tsx
+++ crm-react/src/app/template-syntax/template-syntax.page.tsx
@@ -1,17 +1,21 @@
-export default function TemplateSyntaxPage () {
+export default function TemplateSyntaxPage() {
 
     const info: string = 'Text';
     const valueNum = 234566.09834;
     const status = true;
     const customer = {
-        labelColor : {
+        labelColor: {
             backgroundColor: 'red'
-        }     
+        }
     };
 
     const clickHandler = (value: number): any => {
         console.log(value)
     };
+
+    const list = [
+        'Eier', 'Milch', 'Butter', 'Zucker'
+    ];
 
     return (
         <>
@@ -30,16 +34,16 @@ export default function TemplateSyntaxPage () {
                 valueNum.toLocaleString('de-DE', {
                     style: 'currency',
                     currency: 'EUR'
-                })    
+                })
             }</p>
 
             <h3>Styles</h3>
-            <p style={{color: 'red'}}>Das Syle-Attribute nimmt Variablen entgegen</p>
+            <p style={{ color: 'red' }}>Das Syle-Attribute nimmt Variablen entgegen</p>
             <p style={customer.labelColor}>Label</p>
 
             <h3>Classes</h3>
             <p className="text-xl">Klassen werden über className angegeben</p>
-            <p className={`${(status)? 'text-red-800': 'text-green-800'} bg-sky-300`}>Klassen werden über className angegeben</p>
+            <p className={`${(status) ? 'text-red-800' : 'text-green-800'} bg-sky-300`}>Klassen werden über className angegeben</p>
 
             <h3>Events</h3>
             <div>
@@ -47,6 +51,25 @@ export default function TemplateSyntaxPage () {
                     clickHandler(200);
                 }}>Click</button>
             </div>
+
+            <h3>Listen rendern</h3>
+            <p>Entspricht ngFor, bzw. @for() </p>
+            <ul>
+                {list.map((listItem: string) => {
+                    return (
+                        <li key={listItem}>{listItem}</li>
+                    )
+                })}
+            </ul>
+            <h3>Conditional Rendering</h3>
+            <p>wie ng-if</p>
+            {status ? (
+                <p>Status is true</p>
+            ) : (
+                <p>Status is false</p>
+            )}
+
+            { status && ( <p>OK</p>)}
         </>
     )
 }
