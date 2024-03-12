f85de70fac9cb0ca34b054734555a487809b9aff
Marcus <mk@konitzer-training.de>
2024-03-12 11:30:13 +0100

    syntax

--- crm-react/src/App.tsx
+++ crm-react/src/App.tsx
@@ -1,14 +1,17 @@
 // import './App.css'
 
+import TemplateSyntaxPage from "./app/template-syntax/template-syntax.page"
+
 function App() {
 
 
   return (
     <>
+    {/* Kommentar */}
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
-      <p>hello</p>
+       <TemplateSyntaxPage></TemplateSyntaxPage>
     </>
   )
 }
